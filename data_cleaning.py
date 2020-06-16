48f3c63d4a3cbb3d1530ba33365dd6038aaf64e7
Zhong Xuan <zxkhwa@hotmail.com>
2020-06-16 13:01:56 +0800

    added some documentation

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,28 +1,44 @@
+"""A script used to clean the housing survey results data.
+
+This script cleans the housing preference survery results prior to putting it
+through the matching algorithm. Main goal is to compress the column names and to
+map the raw preference answers (str) onto a scale (int).
+
+The script takes 1 argument, which is the name of the data file to be cleaned.
+The data file should be located in the data/ directory.
+
+Typical usage example (in command line):
+
+python data_cleaning.py raw_data
+"""
+
 import os
 import sys
 import csv
 import pandas as pd
 
 # Get path of raw data, defaulting to raw_uncleaned.csv if left blank.
-if len(sys.argv) == 1: # no arg provided, defaulting
-    raw_path = "data/raw_uncleaned.csv"
+if len(sys.argv) == 1:  # no arg provided, defaulting
+    raw_path = "raw_uncleaned.csv"
 else:
-    if len(sys.argv) >= 3: # too many args, warn + take first argument
+    if len(sys.argv) >= 3:  # too many args, warn + take first argument
         print("Too many arguments. Taking the first arguement as path.")
     raw_path = sys.argv[1]
 print("Extracting raw data from {}.".format(raw_path))
 
 # Import data
 try:
-    raw_data = pd.read_csv(raw_path)
-    raw_data.columns = ["ID", "sex", "country", "sleep",
+    raw_data = pd.read_csv(os.getcwd() + '/data/' + raw_path)
+    raw_data.columns = ["ID", "sex", "school", "country", "sleep",
                         "suite_pref", "cleanliness", "alcohol",
-                        "siblings", "sibs_sameRC", "gender_pref"]
+                        "siblings", "gender_pref"]
 except Exception as inst:
-    print("ERROR: {} does not exist. Please try again.".format(raw_path))
+    print("ERROR: {} does not exist in data/. Please try again.".format(raw_path))
     exit(1)
 
-# Cleaning dictionaries
+# Dictionaries for mapping answers to numbers
+# NOTE: These need to be changed if the questions or answers change
+
 sleep_dict ={"Before 10 PM" : 1,
              "10PM to 12AM" : 2,
              "12AM to 2AM" : 3,
@@ -42,6 +58,7 @@ sibs_clean = {"Yes" : "Yes",
               "No (Proceed to Question 6)" : "No"}
 
 # Clean data
+raw_data["sex"] = [s[0] for s in raw_data["sex"]]
 raw_data["sleep"] = raw_data["sleep"].map(sleep_dict)
 raw_data["suite_pref"] = raw_data["suite_pref"].map(suite_dict)
 raw_data["cleanliness"] = raw_data["cleanliness"].map(cleanliness_dict)
@@ -49,8 +66,8 @@ raw_data["siblings"] = raw_data["siblings"].map(sibs_clean)
 
 # Export data
 PATH_TO_CLEANED = os.getcwd() + "/cleaned"
-try: # Safety check to ensure that data folder exists, and makes it otherwise.
+try:  # Safety check to ensure that data folder exists, and makes it otherwise.
     os.mkdir(PATH_TO_CLEANED)
 except FileExistsError:
     pass
-raw_data.to_csv(PATH_TO_CLEANED + "/" + raw_path)
+raw_data.to_csv(PATH_TO_CLEANED + "/" + raw_path, index=False)
