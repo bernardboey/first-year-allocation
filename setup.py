893a2672f48e4ec15d07d6440805f1123439e19c
Bernard Boey <bbkc22113@gmail.com>
2021-01-05 00:56:04 +0800

    Add setup for freezing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ASAP',
+      version='0.1',
+      description='Allocates Yale-NUS first-years to suites',
+      url='https://github.com/bernardboey/first-year-allocation',
+      author='Bernard Boey',
+      author_email='bernard@u.yale-nus.edu.sg',
+      packages=[''],
+      zip_safe=False)
